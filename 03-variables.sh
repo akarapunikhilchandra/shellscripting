ef7c136f57e704a18dbbac92130fcabab0a97c0d
nikhil <anc@gmail.com>
2023-08-19 15:39:52 +0530

    commiting all

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+echo "my name is devops"
+
+
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1: Hi $PERSON2 how are you?"
+echo "$PERSON2: i m good how are you $PERSON1?"
+echo "$PERSON1: where are you going $PERSON2 ?"
+echo "$PERSON2: i am goog to market ."
