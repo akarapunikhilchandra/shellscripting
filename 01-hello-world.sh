9c4e95b7226f9e8dfdebac30e01567e06fba3492
nikhil <anc@gmail.com>
2023-08-19 15:24:46 +0530

    commiting git

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,3 +1,8 @@
 #!bash/bin
 
 echo "my name is devops"
+
+echo "suresh: Hi ramesh how are you?"
+echo "ramesh: i m goof how are you suresh?"
+echo "suresh: where are you going ramesh ?"
+echo "ramesh: i am goog to market ."
