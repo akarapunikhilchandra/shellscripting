16716455b49609f6b100cfc60c76fcd2427e1694
nikhil <anc@gmail.com>
2023-08-20 18:20:48 +0530

    commiting

--- 09-functions.sh
+++ 09-functions.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 USERID=$(id -u)
+DATE=$(date +%F)
+SCRIPT_NAME=$0
+LOGFILE=/opt/$SCRIPT_NAME-$DATE.log
+
+
+
+
+
 VALIDATE (){
     if [ $1 -ne 0 ] 
 
@@ -11,13 +19,14 @@ else
     echo "$2 .. SUCCESS"
 fi
 }
+USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then 
    echo "ERROR :: Please run with root access"
    exit 1
 fi
 
-yum install mysql -y
+yum install mysql -y &>>$LOGFILE
 VALIDATE $? "Installing MYSQL" 
-yum install postfix -y
+yum install postfix -y &>>$LOGFILE
 VALIDATE $? "Installing postfix"
