23dd8be818eeb94239c24a24a1955a9f691b0647
nikhil <anc@gmail.com>
2023-08-20 12:22:01 +0530

    all

--- 08-mysql.sh
+++ 08-mysql.sh
@@ -3,13 +3,14 @@
 USERID=$(id -u)
 VALIDATE (){
     if [ $1 -ne 0 ] 
-}
+
 then 
     echo "INSTALLATION .. FAILURE"
     exit 1
 else
     echo "INSTALLATION .. SUCCESS"
 fi
+}
 if [ $USERID -ne 0 ]
 then 
    echo "ERROR :: Please run with root access"
