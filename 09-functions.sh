621eb0ae58d22c688c78999ab0bed43a004a6292
nikhil <anc@gmail.com>
2023-08-20 18:36:53 +0530

    commiting

--- 09-functions.sh
+++ 09-functions.sh
@@ -4,9 +4,9 @@ USERID=$(id -u)
 DATE=$(date +%F)
 SCRIPT_NAME=$0
 LOGFILE=/opt/$SCRIPT_NAME-$DATE.log
-R="\e[31m"
-G="\e[32m"
-N="\e[0m"
+R= "\e[31m"
+G= "\e[32m"
+N= "\e[0m"
 VALIDATE (){
     if [ $1 -ne 0 ] 
 
