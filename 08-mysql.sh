b13e3b2d7913c0b443319948543ac8225e183efc
nikhil <anc@gmail.com>
2023-08-20 12:20:55 +0530

    all

---
+++ 08-mysql.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+USERID=$(id -u)
+VALIDATE (){
+    if [ $1 -ne 0 ] 
+}
+then 
+    echo "INSTALLATION .. FAILURE"
+    exit 1
+else
+    echo "INSTALLATION .. SUCCESS"
+fi
+if [ $USERID -ne 0 ]
+then 
+   echo "ERROR :: Please run with root access"
+   exit 1
+fi
+
+yum install mysql -y
+VALIDATE $? "Installing MYSQL" 
+yum install postfix -y
+VALIDATE $? "Installing postfix"
