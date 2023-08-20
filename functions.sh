76dca881b6f38fe8f992610b834cda069ce85331
nikhil <anc@gmail.com>
2023-08-20 18:56:42 +0530

    commiting

---
+++ functions.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+DATE=$(date +%F)
+SCRIPT_NAME=$0
+LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+VALIDATE(){
+if [ $1 -ne 0 ]
+then
+    echo -e "$2 ... $R FAILURE $N"
+    exit 1
+else
+    echo -e "$2 ... $G SUCCESS $N"
+fi
+}
+USERID=$(id -u)
+if [ $USERID -ne 0]
+then
+    echo "ERROR :: Please Run This Script Using Root User"
+    exit 1
+fi
+yum remove mysql -y &>>$LOGFILE
+VALIDATE $? "removing mysql"
+yum remove postfix -y &>>$LOGFILE
+VALIDATE $? "removing mysql"
+
